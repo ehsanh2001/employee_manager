b211f8612006c0af300be6252250dc08faa2e2dc
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-05-17 14:14:29 -0400

    Set the referential action for 'employee' table

--- sql_scripts/create_db.sql
+++ sql_scripts/create_db.sql
@@ -26,6 +26,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
