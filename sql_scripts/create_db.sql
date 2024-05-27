c90e7513c3746f93df47b2f78386557016c50d5e
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-05-26 20:30:19 -0400

    Add consdtraint to not accept empty strings for names and negative salary value

--- sql_scripts/create_db.sql
+++ sql_scripts/create_db.sql
@@ -10,21 +10,21 @@ CREATE DATABASE employee;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) UNIQUE NOT NULL
+    name VARCHAR(100) UNIQUE NOT NULL CHECK (trim(name) <> '')
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(100) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
+    title VARCHAR(100) UNIQUE NOT NULL CHECK (trim(title) <> ''),
+    salary DECIMAL NOT NULL CHECK (salary > 0),
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL CHECK (trim(first_name) <> ''),
+    last_name VARCHAR(30) NOT NULL CHECK (trim(last_name) <> ''),
     role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
